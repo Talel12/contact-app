d33b9af1f94b9ccced32c87075fc526f4ce9a5c4
Talel Ben Belgacem <58850422+Talel12@users.noreply.github.com>
2022-03-20 23:40:06 +0100

    update css

--- front/src/Home.js
+++ front/src/Home.js
@@ -13,7 +13,6 @@ const Home = () => {
     useEffect(() => {
         axios.get("http://localhost:3001/contact").then((res) => {
             setData(res.data)
-            // console.log(data)
         })
     }, [etat]);
 
@@ -21,6 +20,7 @@ const Home = () => {
     const [contactNumber, setContactNumber] = useState();
     const [contactDescription, setContactDescription] = useState({});
     const [contactImage, setContactImage] = useState("");
+    const [search, setSearch] = useState();
 
 
 
@@ -93,55 +93,67 @@ const Home = () => {
                 &&
                 <div className="popUp">
 
-                    <h4>Update Contact</h4>
+                    <h4 className = "title">Update Contact</h4>
                     <label>Name:</label>
                     <input type="text" name="name" defaultValue={selectedItem.contactName} onChange={(event) => setContactName(event.target.value)} />
                     <label>Number:  </label>
                     <input type="number" defaultValue={selectedItem.contactNumber} onChange={(event) => setContactNumber(event.target.value)} />
-                    <label>Number:  </label>
+                    <label>Image Link:  </label>
                     <input type="text" defaultValue={selectedItem.contactImage} onChange={(event) => setContactImage(event.target.value)} />
                     <label>Description:  </label>
                     <input className="textarea" type="textarea" defaultValue={selectedItem.contactDescription} onChange={(event) => setContactDescription(event.target.value)} />
-                    <button onClick={() => update()}>Save</button>
-                    <button onClick={() => reset()}>Cancel</button>
-
+                    <div className="Btn">
+                    <button className ="save" onClick={() => update()}>Save</button>
+                    <button className = "cancl" onClick={() => reset()}>Cancel</button>
+                    </div>
                 </div>}
             {addJeton
                 &&
                 <div className="popUp">
 
-                    <h4>Create Contact</h4>
+                    <h2 className="popUpTitle">Create Contact</h2>
                     <label>Name:</label>
+                
                     <input type="text" defaultValue="" onChange={(event) => setContactName(event.target.value)} />
                     <label>Number:  </label>
                     <input type="number" defaultValue="" onChange={(event) => setContactNumber(event.target.value)} />
-                    <label>Description:  </label>
-                    <label>Number:  </label>
+                    <label>Image Link:  </label>
                     <input type="text" defaultValue="" onChange={(event) => setContactImage(event.target.value)} />
+                    <label>Description:  </label>
                     <input className="textarea" type="textarea" defaultValue="" onChange={(event) => setContactDescription(event.target.value)} />
                     {/* <input type="file" onChange={onFileChange} /> */}
-                    <button onClick={() => createContact()}>Add Contact</button>
-                    <button onClick={() => reset()}>Cancel</button>
-
+                    <div className = "boutton">
+                    <button className = "add" onClick={() => createContact()}>Add Contact</button>
+                    <button className = "cancel" onClick={() => reset()}>Cancel</button>
+                      </div>
                 </div>}
 
 
-            <h1>Contact List </h1>
+            <h1 className="cont">Contact List </h1>
+            <label className="search"> Search </label>
+            <input onChange={(event)=>setSearch(event.value)}/>
             <div className="container">
-                {/* {console.log(data)} */}
-                <button  className="card" onClick={() => setAddJeton(true)}>+</button>
-
+                {/* <FontAwesomeIcon onClick={() => setAddJeton(true)} className="card addBtn" icon="fa-solid fa-circle-plus" />  */}
+                <div className="card borderNone">
+                    <button className=" addBtn" onClick={() => setAddJeton(true)}>+</button>
+                </div>
                 {data.map((el, i) =>
                     <div className="card" key={i}>
-                        <img alt="Picture not Found" src={el.contactImage ? el.contactImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs_Z8NkJTKQmhsyBuGnRsAgd7TiOPuaf7Raw&usqp=CAU"} />
-                        <h3>{el.contactName}</h3>
-                        <h4>{el.contactNumber}</h4>
-                        <p>{el.contactDescription}</p>
-                        <button onClick={() => updateContact(el)}>update</button>
-                        <button
-                            // onClick={() => { if (window.confirm('Are you sure you wish to delete this item?')){deleteContact()} } }
-                            onClick={() => deleteContact(el._id)}
-                        >delete</button>
+                        <div className="cardInfo">
+                            <img alt="Picture not Found" src={el.contactImage ? el.contactImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs_Z8NkJTKQmhsyBuGnRsAgd7TiOPuaf7Raw&usqp=CAU"} />
+                            <div>
+                                <h3>{el.contactName}</h3>
+                                <h4>{el.contactNumber}</h4>
+                                <p>{el.contactDescription}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button onClick={() => updateContact(el)}>update</button>
+                            <button className="red"
+                                // onClick={() => { if (window.confirm('Are you sure you wish to delete this item?')){deleteContact()} } }
+                                onClick={() => deleteContact(el._id)}
+                            >delete</button>
+                        </div>
                     </div>
                 )}
             </div>
